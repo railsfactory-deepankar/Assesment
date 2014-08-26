7438a637b2a7bd1b74cfff67f14979d772e1d362
deepankar <deepankar@railsfactory.org>
2014-08-26 16:28:50 +0530

    Feed

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :carts
 
   get 'store/index'
+  resources :products do
+    get :who_bought, on: :member
+  end
+
 
   resources :products
   root 'store#index', as: 'store'
